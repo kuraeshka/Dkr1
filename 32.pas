283443cca5ce63ca4a299ed68798d1fbb860baf2
kuraeshka <144653643+kuraeshka@users.noreply.github.com>
2023-10-27 07:42:37 +0300

    Add files via upload

---
+++ 32.pas
@@ -0,0 +1,19 @@
+﻿
+var
+  x,y,a,n,b,flag:real;
+  begin
+    n:=-9;
+    b:=5;
+    x:=n;
+    repeat
+    a:=(0.1*x);
+    if x<-7 then y:=60*100-exp(ln(x)*3);
+    if (-7<=x) and (x<3) then y:=(cos(2*x)/tan(x))*(exp(ln(x)*3)/exp(ln(x)*2));
+    if (-4<=x) and (x<3) then y:=(ln(x)/95)-tan(x)/(exp(ln(x)*a));
+    if x>=3 then y:=6-cos(2*x);
+      if y=y then writeln('x=',x:0:4,' ','y=',y:0:4)
+            else writeln('x=',x:0:4,' ','y=корней нет'); 
+      
+    x:=x+1/10;
+    until x>b;
+  end.
